061d8c9302a7b7893c00912c27800bbb9a9cc33c
buanet <andre@buanet.de>
2022-10-04 16:19:51 +0200

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,119 +1,117 @@
-const { defaultTheme } = require('vuepress')
-const { searchPlugin } = require('@vuepress/plugin-search')
+import { defaultTheme } from 'vuepress'
+import { searchPlugin } from '@vuepress/plugin-search'
 
 module.exports = {
   locales: {
-      '/': {
-        lang: 'en-US',
-        title: 'docs.buanet.de',
-        description: 'Learn more about my projects.',
-      },
-      '/de/': {
-        lang: 'de-DE',
-        title: 'docs.buanet.de',
-        description: 'Lerne mehr über meine Projekte.',
-      },
+    '/': {
+      lang: 'en-US',
+      title: 'docs.buanet.de',
+      description: 'Learn more about my projects.',
     },
+    '/de/': {
+      lang: 'de-DE',
+      title: 'docs.buanet.de',
+      description: 'Lerne mehr über meine Projekte.',
+    },
+  },
   theme: defaultTheme({
     locales: {
       '/': {
         selectLanguageName: 'English',
         selectLanguageText: 'Language',
         navbar: [
-          // NavbarItem
           {
             text: 'About me',
             link: '/about/',
           },
-          // NavbarGroup
           {
             text: 'Projects',
             children: [
-              {
-                text: 'Docker Images',
-                children: ['/projects/iobroker-docker-image/readme.md'/*, '/projects/watchdog-docker-image/readme.md', '/projects/pkitool-docker-image/readme.md'*/],
-              },
-//              {
-//               text: 'Raspberry OS Images',
-//                children: ['/projects/iobroker-raspberryos/readme.md', '/projects/docker-raspberryos/readme.md'],
-//              },
-//              {
-//                text: 'Other',
-//                children: ['/projects/php-contact-form/readme.md'],
-//              },
-            ],
-          },
-          {
-            text: 'Docs',
-            children: ['/projects/iobroker-docker-image/docs.md'],
-          },
-//          {
-//            text: 'Tutorials',
-//            children: [
-              //{
-              //  text: 'Docker',
-              //  children: ['/projects/iobroker-docker-image/readme.md', '/projects/watchdog-docker-image/readme.md', '/projects/pkitool-docker-image/readme.md'],
-              //},
-//              {
-//                text: 'ioBroker',
-//                children: ['/tutorials/iobroker/20210114.md'],
-//              },
-//            ],
-//          },
-        ],
-      },
-      '/de/': {
-        selectLanguageName: 'Deutsch',
-        selectLanguageText: 'Sprache',
-        navbar: [
-          // NavbarItem
-          {
-            text: 'Über mich',
-            link: '/de/about/',
-          },
-          {
-            text: 'Projekte',
-            children: [
-              {
-                text: 'Docker Images',
-                children: ['/de/projects/iobroker-docker-image/readme.md'],
-              },
-            ],
-          },
-          {
-            text: 'Doku',
-            children: ['/de/projects/iobroker-docker-image/docs.md'],
-          },
-//          {
-//            text: 'Tutorials',
-//            children: [
-              //{
-              //  text: 'Docker',
-              //  children: ['/projects/iobroker-docker-image/readme.md', '/projects/watchdog-docker-image/readme.md', '/projects/pkitool-docker-image/readme.md'],
-              //},
-//              {
-//                text: 'ioBroker',
-//                children: ['/de/tutorials/iobroker/20210114.md'],
-//              },
-//            ],
-//          },
-          // NavbarGroup
-          //{
-          //  text: 'Tutorials',
-          //  children: [
-          //    {
-          //      text: 'ioBroker',
-          //      link: '/de/tutorials/#iobroker'
-          //    },
-          //  ],
-          // },
-        ],
-      },
+            {
+              text: 'Docker Images',
+              children: ['/projects/iobroker-docker-image/readme.md'/*, '/projects/watchdog-docker-image/readme.md', '/projects/pkitool-docker-image/readme.md'*/],
+            },
+            //              {
+            //               text: 'Raspberry OS Images',
+            //                children: ['/projects/iobroker-raspberryos/readme.md', '/projects/docker-raspberryos/readme.md'],
+            //              },
+            //              {
+            //                text: 'Other',
+            //                children: ['/projects/php-contact-form/readme.md'],
+            //              },
+          ],
+        },
+        {
+          text: 'Docs',
+          children: ['/projects/iobroker-docker-image/docs.md'],
+        },
+        //          {
+        //            text: 'Tutorials',
+        //            children: [
+        //{
+        //  text: 'Docker',
+        //  children: ['/projects/iobroker-docker-image/readme.md', '/projects/watchdog-docker-image/readme.md', '/projects/pkitool-docker-image/readme.md'],
+        //},
+        //              {
+        //                text: 'ioBroker',
+        //                children: ['/tutorials/iobroker/20210114.md'],
+        //              },
+        //            ],
+        //          },
+      ],
     },
-    logo: '/images/logo.png',
-    logoDark: '/images/logo_dark.png',
-    editLink: false,
-    contributors: false,
+    '/de/': {
+      selectLanguageName: 'Deutsch',
+      selectLanguageText: 'Sprache',
+      navbar: [
+        // NavbarItem
+        {
+          text: 'Über mich',
+          link: '/de/about/',
+        },
+        {
+          text: 'Projekte',
+          children: [
+            {
+              text: 'Docker Images',
+              children: ['/de/projects/iobroker-docker-image/readme.md'],
+            },
+          ],
+        },
+        {
+          text: 'Doku',
+          children: ['/de/projects/iobroker-docker-image/docs.md'],
+        },
+        //          {
+        //            text: 'Tutorials',
+        //            children: [
+        //{
+        //  text: 'Docker',
+        //  children: ['/projects/iobroker-docker-image/readme.md', '/projects/watchdog-docker-image/readme.md', '/projects/pkitool-docker-image/readme.md'],
+        //},
+        //              {
+        //                text: 'ioBroker',
+        //                children: ['/de/tutorials/iobroker/20210114.md'],
+        //              },
+        //            ],
+        //          },
+        // NavbarGroup
+        //{
+        //  text: 'Tutorials',
+        //  children: [
+        //    {
+        //      text: 'ioBroker',
+        //      link: '/de/tutorials/#iobroker'
+        //    },
+        //  ],
+        // },
+      ],
+    },
+  },
+  logo: '/images/logo.png',
+  logoDark: '/images/logo_dark.png',
+  editLink: false,
+  contributors: false,
   }),
   plugins: [
     searchPlugin({
@@ -125,7 +123,7 @@ module.exports = {
           placeholder: 'Suche',
         },
       },
-      }),
-    ],
+    }),
+  ],
 }
   
